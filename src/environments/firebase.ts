8fd59d9ef15ce0909bdf520f583cbfdfc951099b
Daniel2506 <danieltrodriguez10@gmail.com>
2020-05-26 20:32:32 -0500

    git ignore

---
+++ src/environments/firebase.ts
@@ -0,0 +1,11 @@
+export const auth = {
+    firebase: {
+        apiKey: 'AIzaSyCbkBeIwziorVz8foC1O-LCihWjTXLVZxs',
+        authDomain: 'platzi-store-625a1.firebaseapp.com',
+        databaseURL: 'https://platzi-store-625a1.firebaseio.com',
+        projectId: 'platzi-store-625a1',
+        storageBucket: 'platzi-store-625a1.appspot.com',
+        messagingSenderId: '738702859024',
+        appId: '1:738702859024:web:4d09fc5a9a6f5d7952a395'
+    }
+};
